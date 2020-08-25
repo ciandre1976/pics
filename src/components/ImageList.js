ef426e4ff9ef17cb020e1d4f32b8d034ae6c1c14
ciandre23 <ciandre23@gmail.com>
2020-08-25 19:43:18 +0200

    adding images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const ImageList=({images})=>{
   const Img=images.map((image)=>{
-    return <img style={{padding:'5px'}} key={image.id} src={image.urls.regular} alt='Img'/>
+    return <img style={{padding:'5px'}} 
+    key={image.id}
+    src={image.urls.regular} alt={image.description}/>
   });
     return <div>{Img}</div>;
 }
